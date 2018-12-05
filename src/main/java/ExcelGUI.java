a96531d9fcd9533984770addfc7ffea7c66292cc
unknown <feytullaev.ars@mail.ru>
2018-12-05 23:18:11 +0400

    Add GUI

--- src/main/java/ExcelGUI.java
+++ src/main/java/ExcelGUI.java
@@ -1,5 +1,13 @@
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
 
 public class ExcelGUI extends JFrame {
 
@@ -12,7 +20,37 @@ public class ExcelGUI extends JFrame {
 
         Container container = this.getContentPane();
         container.add(file1);
+        file1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+
+                    zhepa();
+
+
+
+            }
+        });
     }
+
+    public static void zhepa(){
+        try {
+            File myFile = new File("E://zhepa.xlsx");
+            FileInputStream fis = new FileInputStream(myFile);
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            XSSFCell cell = sheet.getRow(1).getCell(3);
+            cell.setCellValue("Zhepa");
+            FileOutputStream stream = new FileOutputStream(myFile.toString());
+            workbook.write(stream);
+            fis.close();
+            stream.close();
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
 
     public static void main(String[] args){
         ExcelGUI gui = new ExcelGUI();
