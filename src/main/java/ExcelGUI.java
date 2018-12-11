e7675da85f5fde058a390515623f229172ecd597
Фейтуллаев Арсений Валерьевич <FeytullaevAV@gkosar.corp>
2018-12-11 13:47:13 +0400

    Add second analog

--- src/main/java/ExcelGUI.java
+++ src/main/java/ExcelGUI.java
@@ -128,17 +128,27 @@ public class ExcelGUI extends JFrame {
 
 
     public void cian(String dir1, String dir2) {
-        String[] textMain = new String[200];
-        String[] address = new String[200];
-        String[] mr = new String[200];
-        String[] np = new String[200];
-        String[] street = new String[200];
-        String[] snt = new String[200];
-        Float[] square = new Float[200];
-        Integer[] price = new Integer[200];
-        Float[] udel = new Float[200];
-        String[] electro = new String[200];
-        String[] links = new String[200];
+        int rowCount = 0;
+        String[] textMain = new String[300];
+        String[] address = new String[300];
+        String[] mr = new String[300];
+        String[] np = new String[300];
+        String[] street = new String[300];
+        String[] non = new String[300];
+        String[] vzhp = new String[300];
+        String[] snt = new String[300];
+        String[] type = new String[300];
+        String[] naznach = new String[300];
+        double[] square = new double[300];
+        double[] squareLife = new double[300];
+        Integer[] price = new Integer[300];
+        Float[] udel = new Float[300];
+        String[] electro = new String[300];
+        String[] links = new String[300];
+        String[] floor = new String[300];
+        String[] allFloor = new String[300];
+        String[] material = new String[300];
+        String[] houseNum = new String[300];
         bar.setValue(0);
         label3.setText("");
         combo.setEnabled(false);
@@ -159,8 +169,8 @@ public class ExcelGUI extends JFrame {
             XSSFSheet sheet = workbook.getSheetAt(0);
             XSSFCell cell = null;
             XSSFRow row = null;
-
-            for (int i = 1; i < 200; i++) {
+            rowCount = sheet.getPhysicalNumberOfRows();
+            for (int i = 1; i < rowCount; i++) {
                 row = sheet.getRow(i);
                 //Описание
                 cell = row.getCell(getColumnAdress("Описание", sheet));
@@ -176,6 +186,25 @@ public class ExcelGUI extends JFrame {
                 } else {
                     mr[i] = "г " + munR[1];
                 }
+                //Тип
+                cell = row.getCell(getColumnAdress("Тип", sheet));
+                int dasd = getColumnAdress("Тип", sheet);
+                if (cell.getStringCellValue().contains("гараж")) {
+                    type[i] = "Гараж";
+                } else if (cell.getStringCellValue().contains("квартиры")) {
+                    type[i] = "Квартира";
+                } else if (cell.getStringCellValue().contains("комнтаы")) {
+                    type[i] = "Комната";
+                } else if (cell.getStringCellValue().contains("помещения")) {
+                    type[i] = "ПСН";
+                } else if (cell.getStringCellValue().contains("офиса")) {
+                    type[i] = "Офис";
+                } else if (cell.getStringCellValue().contains("участка")) {
+                    type[i] = "Участок";
+                } else if (cell.getStringCellValue().contains("земли")) {
+                    type[i] = "Земля";
+                }
+
 
 
                 //Парс НП
@@ -198,58 +227,128 @@ public class ExcelGUI extends JFrame {
                 }
                 System.out.println(i);
                 //Парс улицы
-                if (munR.length > 2) {
-                    if (munR[2].contains("улица") || munR[2].contains("проезд") || munR[2].contains("тупик")) {
-                        street[i] = munR[2];
-                    }
-                } else if (munR.length > 2) {
-                    if (munR[3].contains("улица") || munR[3].contains("проезд") || munR[3].contains("тупик")) {
-                        street[i] = munR[3];
-                    }
-                } else if (munR.length > 3) {
-                    if (munR[4].contains("улица") || munR[4].contains("проезд") || munR[4].contains("тупик")) {
-                        street[i] = munR[4];
+                for (int j = 0; j < munR.length; j++){
+                    if (munR[j].contains("улица")){
+                        munR[2] = munR[2].replace("улица", "");
+                        street[i] = "ул " + munR[2].trim();
+                    } else if (munR[j].contains("проезд")) {
+                        munR[2] = munR[2].replace("проезд", "");
+                        street[i] = "проезд " + munR[2].trim();
+                    } else if (munR[j].contains("тупик")) {
+                        munR[2] = munR[2].replace("тупик", "");
+                        street[i] = "тупик " + munR[2].trim();
                     }
                 }
+                //Номер дома
+                String number = munR[munR.length - 1];
+                if (number.matches("[а-яА-я0-9/]+") && number.length() < 8) {
+                    houseNum[i] = number;
+                }
+
+                //Этажи и материал стен
+                if (type[i].equals("Квартира") || type[i].equals("Комната")){
+                    cell = row.getCell(getColumnAdress("Дом", sheet));
+                    String[] floors = cell.getStringCellValue().split(", ");
+                    String[] floorsNew = floors[0].split("/");
+                    if (floorsNew[0] != null){
+                        floor[i] = floorsNew[0];
+                    }
+                    if (floorsNew[1] != null){
+                        allFloor[i] = floorsNew[1];
+                    }
+                    if (floors.length > 1){
+                        material[i] = floors[1].replace("й","е");
+                    }
+
+                } else if (getColumnAdress("Этаж", sheet) != 0){
+                    cell = row.getCell(getColumnAdress("Этаж", sheet));
+                    String[] floors = cell.getStringCellValue().split("/");
+                    floor[i] = floors[0];
+                    if (floors.length > 1){
+                        allFloor[i] = floors[1];
+                    }
+                }
+
+
+//                if (munR.length > 2) {
+//                    if (munR[2].contains("улица") || munR[2].contains("проезд") || munR[2].contains("тупик")) {
+//                        munR[2] = munR[2].replace("улица", "");
+//                        street[i] = "ул " + munR[2].trim();
+//                    }
+//                } else if (munR.length > 2) {
+//                    if (munR[3].contains("улица") || munR[3].contains("проезд") || munR[3].contains("тупик")) {
+//                        street[i] = munR[3];
+//                    }
+//                } else if (munR.length > 3) {
+//                    if (munR[4].contains("улица") || munR[4].contains("проезд") || munR[4].contains("тупик")) {
+//                        street[i] = munR[4];
+//                    }
+//                }
                 //СНТ
                 int columnSNT = 0;
-                if ((getColumnAdress("Название коттеджного поселка", sheet) != 0)){
+                if ((getColumnAdress("Название коттеджного поселка", sheet) != 0)) {
                     cell = row.getCell(getColumnAdress("Название коттеджного поселка", sheet));
                     snt[i] = cell.getStringCellValue();
                 }
+                //НОН
+                if (type[i].equals("Квартира") || type[i].equals("Комната")) {
+                    non[i] = "Жилое помещение";
+                } else {
+                    non[i] = "Нежилое помещение";
+                }
+                //ВЖП
+                if (type[i] != null) {
+                    if (type[i].equals("Квартира") || type[i].equals("Комната"))
+                        vzhp[i] = type[i];
+                }
+                //Назначение здания
+                if (type[i].equals("Квартира") || type[i].equals("Комната")){
+                    naznach[i] = "Многоквартирный дом";
+                } else {
+                    naznach[i] = "Нежилое";
+                }
 
-                //Площадь для участков
+                //Площадь
                 int columnSquare = 0;
-                if ((getColumnAdress("Участок", sheet) != 0)) {
+                if (getColumnAdress("Участок", sheet) != 0) {
                     columnSquare = getColumnAdress("Участок", sheet);
-                } else {
+                    square[i] = getSquare(columnSquare, row);
+                } else if (getColumnAdress("Площадь", sheet) != 0){
                     columnSquare = getColumnAdress("Площадь", sheet);
-                }
-                cell = row.getCell(columnSquare);
-                String[] parse = cell.getStringCellValue().split(", ");
-                if (parse.length == 2) {
-                    if (NumberUtils.isCreatable(parse[0])) {
-                        if (parse[1].equals("сот.")) {
-                            square[i] = Float.parseFloat(parse[0]) * 100;
-                        } else if (parse[1].equals("га")) {
-                            square[i] = Float.parseFloat(parse[0]) * 1000;
-                        } else {
-                            square[i] = Float.parseFloat(parse[0]);
+                    square[i] = getSquare(columnSquare, row);
+                } else  {
+                    columnSquare = getColumnAdress("Площадь, м2", sheet);
+                    cell = row.getCell(columnSquare);
+                    String[] parse = cell.getStringCellValue().split("/");
+                    if (type[i].equals("Квартира")){
+                        if (parse.length > 1){
+                            square[i] = Double.parseDouble(parse[0]);
+                            squareLife[i] = Double.parseDouble(parse[1]);
+                        }else {
+                            square[i] = Double.parseDouble(parse[0]);
+                            squareLife[i] = Double.parseDouble(parse[0]);
                         }
-                    } else {
-                        square[i] = Float.valueOf(0);
+                    }else if (type[i].equals("Комната")){
+                        if (parse.length > 1){
+                            square[i] = Double.parseDouble(parse[1]);
+                            squareLife[i] = Double.parseDouble(parse[0]);
+                        }else {
+                            square[i] = Double.parseDouble(parse[0]);
+                            squareLife[i] = Double.parseDouble(parse[0]);
+                        }
+                    }else {
+                        square[i] = Double.parseDouble(parse[0]);
                     }
-                } else if (NumberUtils.isCreatable(parse[0])) {
-                    square[i] = Float.parseFloat(parse[0]);
-                } else {
-                    square[i] = (float) 0;
+
+
                 }
+
 
                 //Цена
                 cell = row.getCell(getColumnAdress("Цена", sheet));
-                parse = cell.getStringCellValue().split(" ");
+                String[] parse = cell.getStringCellValue().split(" ");
                 price[i] = Integer.parseInt(parse[0]);
-                udel[i] = price[i] / square[i];
+//                udel[i] = price[i] / square[i];
                 //Доп
                 cell = row.getCell(getColumnAdress("Дополнительно", sheet));
                 electro[i] = cell.getStringCellValue();
@@ -275,17 +374,63 @@ public class ExcelGUI extends JFrame {
             sheet = workbook.getSheetAt(0);
             try {
 
-                for (int i = 1; i < 200; i++) {
+                for (int i = 1; i < rowCount; i++) {
                     row = sheet.getRow(i);
                     //Описание
                     cell = row.getCell(getColumnAdress("Текст объявления", sheet));
                     cell.setCellValue(textMain[i]);
+                    //Назначение
+                    if (getColumnAdress("Назначение объекта недвижимости",sheet) != 0){
+                        if (non[i] != null){
+                            cell = row.getCell(getColumnAdress("Назначение объекта недвижимости", sheet));
+                            cell.setCellValue(non[i]);
+                        }
+                    }
+                    //Вид жилого помещ
+                    if (getColumnAdress("Вид жилого помещения",sheet) != 0){
+                        if (vzhp[i] != null){
+                            cell = row.getCell(getColumnAdress("Вид жилого помещения", sheet));
+                            cell.setCellValue(vzhp[i]);
+                        }
+                    }
+                    //Назначение здания
+                    if (getColumnAdress("Назначение здания, в котором расположено помещение",sheet) != 0){
+                        if (naznach[i] != null){
+                            cell = row.getCell(getColumnAdress("Назначение здания, в котором расположено помещение", sheet));
+                            cell.setCellValue(naznach[i]);
+                        }
+                    }
                     //Адрес
                     cell = row.getCell(getColumnAdress("Адрес объекта недвижимости (по объявлению)", sheet));
                     cell.setCellValue(address[i]);
                     //Площадь
-                    cell = row.getCell(getColumnAdress("Площадь, кв.м.", sheet));
+                    cell = row.getCell(getColumnAdress("Площадь, кв.м. ", sheet));
                     cell.setCellValue(square[i]);
+                    if (getColumnAdress("Жилая площадь кв.м.", sheet) != 0){
+                        cell = row.getCell(getColumnAdress("Жилая площадь кв.м.", sheet));
+                        cell.setCellValue(squareLife[i]);
+                    }
+                    //Главная ссылка
+
+                    //Материал стен
+                    if (getColumnAdress("Материал стен ", sheet) != 0){
+                        cell = row.getCell(getColumnAdress("Материал стен ", sheet));
+                        cell.setCellValue(material[i]);
+                    }
+                    //Этажи
+                    if (getColumnAdress("Номер этажа", sheet) != 0){
+                        cell = row.getCell(getColumnAdress("Номер этажа", sheet));
+                        cell.setCellValue(floor[i]);
+                    }
+                    if (getColumnAdress("Количество этажей здания", sheet) != 0){
+                        cell = row.getCell(getColumnAdress("Количество этажей здания", sheet));
+                        cell.setCellValue(allFloor[i]);
+                    }
+                    //Номер дома
+                    if (getColumnAdress("Номер дома", sheet) != 0){
+                        cell = row.getCell(getColumnAdress("Номер дома", sheet));
+                        cell.setCellValue(houseNum[i]);
+                    }
                     //Цена
                     cell = row.getCell(getColumnAdress("Полная цена (из источника)", sheet));
                     cell.setCellValue(price[i]);
@@ -382,11 +527,34 @@ public class ExcelGUI extends JFrame {
         }
         for (int i = 0; i < columnSize; i++) {
             Cell c = row.getCell(i);
-            if (c.getStringCellValue().equals(name)) {
+            if (c.getStringCellValue().equals(name)){
                 count = i;
             }
         }
         return count;
+    }
+
+
+    public double getSquare(int columnSquare, Row row) {
+        Cell cell = row.getCell(columnSquare);
+        String[] parse = cell.getStringCellValue().split(", ");
+        if (parse.length == 2) {
+            if (NumberUtils.isCreatable(parse[0])) {
+                if (parse[1].equals("сот.")) {
+                    return Double.parseDouble(parse[0]) * 100;
+                } else if (parse[1].equals("га")) {
+                    return Double.parseDouble(parse[0]) * 1000;
+                } else {
+                    return Float.parseFloat(parse[0]);
+                }
+            } else {
+                return Float.valueOf(0);
+            }
+        } else if (NumberUtils.isCreatable(parse[0])) {
+            return Float.parseFloat(parse[0]);
+        } else {
+            return (float) 0;
+        }
     }
 
 
